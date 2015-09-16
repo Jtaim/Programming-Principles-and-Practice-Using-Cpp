3d538e54ca92d32c11ab4fe3dad1c4dd22fa36f2
Jtaim <jtaim@users.noreply.gethub.com>
2015-09-15 23:14:03 -0400

    added better comments in the source code

--- Hello World/HelloWorld.cpp
+++ Hello World/HelloWorld.cpp
@@ -1,3 +1,9 @@
+//Written by Jtaim
+//Date 15 Sept 2015
+//Programming: Principles and Practice Using C++ Second Edition
+
+// simple hello world program
+
 //#include "../std_lib_facilities.h"  // added the ../ because up a level from source files
 #include<iostream>
 #include<string>
@@ -8,9 +14,11 @@
 using namespace std;
 inline void keep_window_open() { char ch; cin >> ch; }
 
+//C++ programs start by executing the function main
 int main()
 {
-	cout << "Hello, World!\n";
-	keep_window_open();
+	cout << "Hello, World!\n"		//output "Hello, World!"
+		 << "Here we go again.\n";
+	keep_window_open();				//wait for a character to be entered
 	return 0;
 }
--- Name Age 3_3 try/NameAge.cpp
+++ Name Age 3_3 try/NameAge.cpp
@@ -1,4 +1,17 @@
-//#include "../std_lib_facilities.h"  // added the ../ because up a level from source files
+//written by Jtaim
+//date 15 Sept 2015
+//Programming: Principles and Practice Using C++ Second Edition
+
+/*
+Section 3.3 try this exercise
+Get the “name and age” example to run.
+Then, modify it to write out the age in months:
+read the input in years and multiply (using the * operator) by 12.
+Read the age into a double to allow for children who can be very
+proud of being five and a half years old rather than just five.
+*/
+
+//#include "std_lib_facilities.h" //custom header
 #include<iostream>
 #include<string>
 #include<vector>
@@ -8,15 +21,17 @@
 using namespace std;
 inline void keep_window_open() { char ch; cin >> ch; }
 
+//C++ programs start by executing the function main
 int main()
 {
-	cout << "Enter name and age\n";
-	string name = "???";
-	int age = 0;
-	cin >> name >> age;
-	cout << "Hi " << name << " you are " << age << "?\n";
-
-
-	keep_window_open();
+	cout << "Please enter your first name and age\n";
+	string first_name = "???"; 		//string variable
+									//("???" means "don't know the name")
+	double age = -1;				//integer variable (-1 means "don't know the age")
+	cin >> first_name >> age;
+	double age_months = age * 12;
+	cout << "Hello, " << first_name
+		 << " (age " << age << ")"
+		 << "(age in months " << age_months << ")\n";
 	return 0;
 }

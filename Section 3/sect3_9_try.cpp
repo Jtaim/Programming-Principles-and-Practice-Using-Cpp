9cbc1de1e1b6a1cae56887c0ec5c19a226f6c8aa
Jtaim <jtaim@users.noreply.gethub.com>
2015-09-19 13:38:57 -0400

    more stuff

--- Section 3/sect3_9_try.cpp
+++ Section 3/sect3_9_try.cpp
@@ -16,17 +16,16 @@ using namespace std;
 inline void keep_window_open() { char ch; cin >> ch; }
 
 //C++ programs start by executing the function main
+//exit EOF ctrl-z
 int main()
 {
 	double d = 0;
-    while (cin>>d) {            		// repeat the statements below
-                                        // as long as we type in numbers
-        //int i {d};              		// try to squeeze a double into an int
-										// {} is new in C++ 11 UNIVERSAL AND UNIFORM INITIALIZATION compiler warning
-		int i = narrow_cast<int>(d);	// talked about in 5.6.4 narrowing errors 
-		//char c {i};            			// try to squeeze an int into a char
-		char i = narrow_cast<char>(i);
-        int i2 = c;             		// get the integer value of the character
+    while (cin>>d)	            		
+	{
+        int i = d;						// try to squeeze a double into an int
+		//int i {d};						// the {} enforce error narrowing error
+		char c = i;            			// try to squeeze an int into a char
+		int i2 = c;             		// get the integer value of the character
         cout << "d==" << d      		// the original double
              << " i=="<< i      		// converted to int
              << " i2==" << i2   		// int value of char

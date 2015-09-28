b40fe31938985f6a010aecb94c432b1c6d480299
Jtaim <jtaim@users.noreply.gethub.com>
2015-09-27 21:04:47 -0400

    done with section 4 drills

--- Section 4/sect4_drill11.cpp
+++ Section 4/sect4_drill11.cpp
@@ -3,7 +3,7 @@
 //Programming: Principles and Practice Using C++ Second Edition
 
 /*
-Section 4 Drill step 7 and 8.
+Section 4 Drill step 9 - 11.
 1. Write a program that consists of a while-loop that (each time around the loop)
    reads in two int numbers and then prints them. Exit the program when a terminating '|' is entered.
 2. Change the program to write out the smaller value is: followed by the smaller of the
@@ -24,15 +24,28 @@ Section 4 Drill step 7 and 8.
    You may consider 12 m (with a space between the number and the unit) equivalent to 12m (without a space).
 8. Reject values without units or with “illegal” representations of units, 
    such as y, yard, meter, km, and gallons.
+9. Keep track of the sum of values entered (as well as the smallest and the largest)
+   and the number of values entered. When the loop ends, print the smallest, the largest,
+   the number of values, and the sum of values. Note that to keep the sum, you have to
+   decide on a unit to use for that sum; use meters.
+10.Keep all the values entered (converted into meters) in a vector.
+   At the end, write out those values.
+11.Before writing out the values from the vector, sort them (that’ll make them come out
+   in increasing order).
 */
 
 #include "section4.h" //custom header
-void conversion(string, double, const vector<string>);
+double convert_meter(string, double);
 
 int main()
 {
     double inNum{0};
     string inUnit{""};
+	vector<double> entered_values{};
+	double small = 0;
+	double large = 0;
+	const double tol = 1.0 / 1000;  //close enough
+	double sum = 0;
     bool stop = false;  //loop control
     const vector<string> approved_units{"cm", "m", "in", "ft"};  // approved units
     cout << "Enter a number with unit of measure (cm, m, in, ft).\n";
@@ -41,7 +54,7 @@ int main()
     {
         cin >> inNum;
         if(cin.eof())  // added so EOF is captured
-            return 0;  // for now just just exit program
+            return 0;  // for now just exit program
         else
         {
             cin.clear();
@@ -69,7 +82,22 @@ int main()
                 }
 				if (good_unit)
 				{
-					conversion(inUnit, inNum, approved_units);
+					double temp = convert_meter(inUnit, inNum);
+					sum += temp;
+					if (temp < small || temp > large)  // check if have new smaller or larger value
+					{
+						if (((temp - small) < 0 && fabs(temp - small) > tol) || small == 0)
+						{
+							small = temp;
+							cout << " This is the smallest number entered so far!\n\n";
+						}
+						if (((temp - large) > 0 && fabs(temp - large) > tol) || large == 0)
+						{
+							large = temp;
+							cout << " This is the largest number entered so far!\n\n";
+						}
+					}
+					entered_values.push_back(temp);
 					inNum = 0;
 					inUnit = {""};
 				}
@@ -82,57 +110,27 @@ int main()
             }
         }
     }
+	inUnit = "m";
+	cout << small << inUnit << " was the smallest number entered.\n";
+	cout << large << inUnit << " was the largest number entered.\n";
+	cout << sum << inUnit << " was the sum of all the numbers entered.\n\n";
+	sort(entered_values.begin(),entered_values.end());
+	for (double i : entered_values)
+		cout << i << inUnit << endl;
+	keep_window_open();
     return 0;
 }
-// Print the correct entries plus the converted value for other units
 // Assume conversion factors
-// 1m == 100cm, 1in == 2.54cm, 1ft == 12in. Read the unit indicator into a string.
-void conversion(string unit, double val, const vector<string> units)
+// 1m == 100cm, 1in == 2.54cm, 1ft == 12in.
+double convert_meter(string unit, double val)
 {
-    double meter = 1;
-    double centimeter = 1;
-    double feet = 1;
-    double inch = 1;
-    if(unit == "m")
-    {
-        meter = val;
-        centimeter = meter * 100;
-        inch = centimeter / 2.54;
-        feet = inch / 12;
-    }
-    else if(unit == "cm")
-    {
-        centimeter = val;
-        meter = centimeter / 100;
-        inch = centimeter / 2.54;
-        feet = inch / 12;
-    }
-    else if(unit == "in")
-    {
-        inch = val;
-        feet = inch / 12;
-        centimeter = inch * 2.54;
-        meter = centimeter / 100;
-    }
-    else if(unit == "ft")
-    {
-        feet = val;
-        inch = feet * 12;
-        centimeter = inch * 2.54;
-        meter = centimeter / 100;
-    }
-    else
-        cerr << "error no such unit exists to convert.\n";
-    for(string converts : units)
-    {
-        if(converts == "m")
-            val = meter;
-        else if(converts == "cm")
-            val = centimeter;
-        else if(converts == "ft")
-            val = feet;
-        else if(converts == "in")
-            val = inch;
-        cout << "Entered measurement is " << val << " " << converts << endl;
-    }
+    if (unit == "cm")
+		val /= 100;
+	if (unit == "m")			//put all units in units of meters (m)
+		val *= 1.0;
+	if (unit == "in")
+		val = val*2.54 / 100;
+	if (unit == "ft")
+		val = val * 12 * 2.54 / 100;
+	return val;
 }

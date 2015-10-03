ebf1e5456d8210c4729446e7bdfe7237db0ea844
Jtaim <Jtaim@users.noreply.github.com>
2015-10-02 20:37:32 -0400

    Rename sect4_drill11.cpp to sect4_drill9_11.cpp

--- Section 4/sect4_drill9_11.cpp
+++ Section 4/sect4_drill9_11.cpp
@@ -16,13 +16,13 @@ Section 4 Drill step 9 - 11.
 6. Now change the body of the loop so that it reads just one double each time around.
    Define two variables to keep track of which is the smallest and which is the largest
    value you have seen so far. Each time through the loop write out the value entered.
-   If itís the smallest so far, write the smallest so far after the number.
+   If it‚Äôs the smallest so far, write the smallest so far after the number.
    If it is the largest so far, write the largest so far after the number.
 7. Add a unit to each double entered; that is, enter values such as 10cm, 2.5in, 5ft, 
    or 3.33m. Accept the four units: cm, m, in, ft. Assume conversion factors
    1m == 100cm, 1in == 2.54cm, 1ft == 12in. Read the unit indicator into a string.
    You may consider 12 m (with a space between the number and the unit) equivalent to 12m (without a space).
-8. Reject values without units or with ìillegalî representations of units, 
+8. Reject values without units or with ‚Äúillegal‚Äù representations of units, 
    such as y, yard, meter, km, and gallons.
 9. Keep track of the sum of values entered (as well as the smallest and the largest)
    and the number of values entered. When the loop ends, print the smallest, the largest,
@@ -30,7 +30,7 @@ Section 4 Drill step 9 - 11.
    decide on a unit to use for that sum; use meters.
 10.Keep all the values entered (converted into meters) in a vector.
    At the end, write out those values.
-11.Before writing out the values from the vector, sort them (thatíll make them come out
+11.Before writing out the values from the vector, sort them (that‚Äôll make them come out
    in increasing order).
 */
 

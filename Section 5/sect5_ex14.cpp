a3bd4dd1db9f6a8a5b939869fa35866eda612ba2
Jtaim <jtaim@users.noreply.gethub.com>
2015-11-11 01:00:06 -0500

    fixed section 5 exercise 14 bugs

--- Section 5/sect5_ex14.cpp
+++ Section 5/sect5_ex14.cpp
@@ -24,32 +24,52 @@ struct days {
 	{ {},"Saturday" }
 	};
 };
+int sum(pair<vector<int>, string> &day_values) {
+	int sum{ 0 };
+	for (auto i : day_values.first) {
+		if ((i > 0) && (sum > INT_MAX - i)) {
+			error("int max overflow error");
+		}
+		if ((i < 0) && (sum < INT_MAX - i)) {
+			error("int min overflow error");
+		}
+		sum += i;
+	}
+	return sum;
+}
 
-bool handle_input(string, int, int&, days&);
-void print_results(const int, const days&);
+int handle_input(string, int, days&);
 
 int main()
 try 
 {
-	bool escape{ true };
 	days weekdays;
 	string day{};
 	int value{ 0 };
 	int rejects{ 0 };
-	cout << "Enter days of the week and a value. Enter q 5 to exit\n";
+	cout << "Enter days of the week and a value. Enter q to exit\n";
+	bool escape{ true };
 	while (escape) {
-		cin >> day >> value;
-		if (cin.good()) {
-			escape = handle_input(day, value, rejects, weekdays);
-			if (!escape) {
-				print_results(rejects, weekdays);
+		cin >> day;
+		if (day == "q") {
+			escape = false;
+		}
+		if (escape) {
+			cin >> value;
+			if (cin.bad() || cin.eof() || cin.fail()) {
+				escape = false;
+				error("Got some bad input day values.\n");
+			}
+			else {
+				rejects += handle_input(day, value, weekdays);
 			}
 		}
-		else  // something bad was entered
-			escape = false;
-	}
-	if (cin.fail()) {
-		error("Got some bad input.\n");
+		if (!escape) { //print the results
+			for (auto weekday : weekdays.d) { //get vector of pairs from the struct
+				cout << weekday.second << " has a sum of " << sum(weekday) << '\n';
+			}
+			cout << "The number of rejects was " << rejects << "\n\n";
+		}
 	}
 	keep_window_open();
 	return 0;
@@ -65,41 +85,33 @@ catch (...) {
 	return 2;
 }
 
-bool handle_input(string day, int val, int &rejected, days &week)
+int handle_input(string day, int val, days &week)
 {
-	bool condition{ true };
-	for (string::size_type i = 0; i < day.length(); ++i)
+	for (string::size_type i = 0; i < day.size(); ++i)
 		tolower(day[i]);
-	if (day == "sunday" || day == "sun")
+	if (day == "sunday" || day == "sun") {
 		week.d[0].first.push_back(val);
-	else if (day == "monday" || day == "mon")
-		week.d[1].first.push_back(val);
-	else if (day == "tuesday" || day == "tue")
-		week.d[2].first.push_back(val);
-	else if (day == "wednesday" || day == "wed")
-		week.d[3].first.push_back(val);
-	else if (day == "thursday" || day == "thu")
-		week.d[4].first.push_back(val);
-	else if (day == "friday" || day == "fri")
-		week.d[5].first.push_back(val);
-	else if (day == "saturday" || day == "sat")
-		week.d[6].first.push_back(val);
-	else if (day == "q" && val == 5)
-		condition = false;
-	else ++rejected;
-	return condition;
-}
-void print_results(const int rej, const days &wds)
-{
-	for (auto weekday : wds.d) { //get to vector from the struct
-		cout << weekday.second << " has a sum of "; // second in the pair is the name of the day
-		int sum = 0;
-		for (auto values : weekday.first) { // get to the vector holding numbers for the day
-			sum += values;
-		}
-		cout << sum << '\n';
 	}
-	cout << "the number of rejects was " << rej << "\n\n";
-	cout << "what a long trip it has been.\n";
-	return;
+	else if (day == "monday" || day == "mon") {
+		week.d[1].first.push_back(val);
+	}
+	else if (day == "tuesday" || day == "tue") {
+		week.d[2].first.push_back(val);
+	}
+	else if (day == "wednesday" || day == "wed") {
+		week.d[3].first.push_back(val);
+	}
+	else if (day == "thursday" || day == "thu") {
+		week.d[4].first.push_back(val);
+	}
+	else if (day == "friday" || day == "fri") {
+		week.d[5].first.push_back(val);
+	}
+	else if (day == "saturday" || day == "sat") {
+		week.d[6].first.push_back(val);
+	}
+	else {
+		return 1;
+	}
+	return 0;
 }

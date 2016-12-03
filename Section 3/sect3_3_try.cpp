737040521a570a231a841fe0ec1bc7dda6fe6f4e
Jtaim <Jtaim@users.noreply.github.com>
2016-12-02 22:07:23 -0500

    Update sect3_3_try.cpp
    minor edit

--- Section 3/sect3_3_try.cpp
+++ Section 3/sect3_3_try.cpp
@@ -21,8 +21,8 @@ int main()
 {
 	std::cout << "Please enter your first name and age\n";
 	std::string first_name = "???"; 		//string variable
-											//("???" means "don't know the name")
-	double age = -1;						//integer variable (-1 means "don't know the age")
+							//("???" means "don't know the name")
+	double age = -1;				//integer variable (-1 means "don't know the age")
 	std::cin >> first_name >> age;
 	double age_months = age * 12;
 	std::cout << "Hello, " << first_name

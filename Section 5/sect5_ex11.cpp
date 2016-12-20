0f17692cf2adabedfd9fcccc717bb79af885f1ed
Jtaim <jtaim@users.noreply.gethub.com>
2016-12-20 08:00:42 -0500

    review and refactor

--- Section 5/sect5_ex11.cpp
+++ Section 5/sect5_ex11.cpp
@@ -1,5 +1,6 @@
 //written by Jtaim
 //date 6 Nov 2015
+//updated 20 Dec 2016
 //Programming Principles and Practice Using C++ Second Edition, Bjarne Stroustrup
 
 /*
@@ -9,44 +10,48 @@ A program that writes out the Fibonacci series up to largest to fit in an int ty
 
 #include "section5.h"
 
-int sum(int, int);
+unsigned int sum(unsigned int, unsigned int);
 
 int main()
 try
 {
-	vector<int> fibonacci{ 0, 1 };
-	for (int i = 0; ; ++i) {
-		//cout << fibonacci[i+1] << '\n';
-		fibonacci.push_back(sum(fibonacci[i], fibonacci[i+1]));
+	std::vector<unsigned int> fibonacci{ 0, 1 };
+	for (unsigned int i = 0; ; ++i)
+	{
+		std::cout << fibonacci[i+1] << '\n';
+		fibonacci.push_back(sum(fibonacci[i], fibonacci[i + 1]));
 	}
-	keep_window_open();
+	keep_window_open();	// yes I know will never get to this point
 	return 0;
 }
-catch (exception& e) 
+catch (std::exception& e)
 {
-	cerr << "error: " << e.what() << '\n';
+	std::cerr << "error: " << e.what() << '\n';
 	keep_window_open();
 	return 1;
 }
-catch (...) 
+catch (...)
 {
-	cerr << "Oops: unknown exception!\n";
+	std::cerr << "Oops: unknown exception!\n";
 	keep_window_open();
 	return 2;
 }
-/*	Sums 2 integer values.
-Inputs:		int, int
+
+/*	Sums 2 unsigned integer values.
+Inputs:		unsigned int, unsigned int
 Outputs:	sum
-Errors:		if the sums overflow int type
+Errors:		if the sums overflow unsigned int type
 */
-int sum(int var1, int var2)
+unsigned int sum(unsigned int var1, unsigned int var2)
 {
-	if ((var2 > 0) && (var1 > INT_MAX - var2)) {
-		cout << var2 << " is the max Fibonacci series number to fit in an int type.\n";
-		error("int max overflow error");
+	if ((var2 > 0) && (var1 > UINT_MAX - var2))
+	{
+		std::cout << var2 << " is the max Fibonacci series number to fit in an unsigned int type.\n";
+		error("unsigned int max overflow error");
 	}
-	//if ((var2 < 0) && (var1 < INT_MAX - var2)) {
-	//	error("int min overflow error");
+	//if ((var2 < 0) && (var1 < UINT_MAX - var2))
+	//{
+	//	error("unsigned int min overflow error");
 	//}
 	return var1 + var2;
 }

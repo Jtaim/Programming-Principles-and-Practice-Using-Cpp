6d927cf85e2ca4dad333c9ce8e8e0b39b12868d0
Jtaim <jtaim@users.noreply.gethub.com>
2017-03-20 21:33:17 -0400

    Working in new release of microsoft community

--- Section 2/HelloWorld.cpp
+++ Section 2/HelloWorld.cpp
@@ -1,18 +1,15 @@
 //Written by Jtaim
-//Date 15 Sept 2015
-//Updated 1 Dec 2016
+//Date 20 March 2017
 //Programming: Principles and Practice Using C++ Second Edition
-
 // simple hello world program
 
 #include<iostream>
 
-//C++ programs start by executing the function main
-//use std to used the standard library could also use using namespace std;
+using namespace std;
+
 int main()
 {
-	std::cout << "Hello, World!\n"		//output "Hello, World!"
-		<< "Here we go again.\n";
-	std::cin.get();						//wait for a character to be entered
+	cout << "Hello, World!\nC++ is fun :)\n";
+	std::cin.get();	//wait for a character to be entered
 	return 0;
 }

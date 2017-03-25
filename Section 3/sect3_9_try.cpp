0a5b04fd3c527b73efa79ab9662cd7b51dd1c5b0
Jtaim <jtaim@users.noreply.gethub.com>
2017-03-24 20:00:02 -0400

    refresh

--- Section 3/sect3_9_try.cpp
+++ Section 3/sect3_9_try.cpp
@@ -1,32 +1,29 @@
 //written by Jtaim
-//date 18 Sept 2015
-//updated 1 Dec 2016
+//date 24 Mar 2017
 //Programming: Principles and Practice Using C++ Second Edition
 /*
 Section 3.9 try this exercise
 */
 
-#include <iostream>
-#include <string>
 #include "section3.h" //custom header
 
-//C++ programs start by executing the function main
-//exit EOF ctrl-z
+//exit EOF or ctrl-z
 int main()
 {
 	using namespace std;
-	double d = 0;
-	std::cout << "show some basic of how C++ converts types. EOF to exit\n";
+
+	double d{ 0.0 };
+	cout << "show some basic of how C++ converts types. EOF to exit\n";
 	while (cin >> d)
 	{
-		int i = d;						// try to squeeze a double into an int
-		//int i {d};						// the {} enforce error narrowing error
-		char c = i;            			// try to squeeze an int into a char
+		int i{ static_cast<int>(d) };	// try to squeeze a double into an int
+		//int j {d};						// the {} enforce error narrowing error
+		char c{ static_cast<char>(i) }; // try to squeeze an int into a char
 		int i2 = c;             		// get the integer value of the character
 		cout << "the entered double value " << d << "\n"
-			<< "double converted to int " << i << "\n"
-			<< "int converted to char (" << c << ")\n"
-			<< "char converted to int " << i2 << "\n";
+			 << "double converted to int " << i << "\n"
+			 << "int converted to char (" << c << ")\n"
+			 << "char converted to int " << i2 << "\n";
 	}
 	return 0;
 }
